badadd4590754493e7219924fac93314308b5755
CelineRondeau <rondeau9314@gmail.com>
2020-05-26 16:20:50 -0400

    Added routes to events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,12 @@ class EventsController < ApplicationController
 
   def index
     # => GET    /events
+    if params[:query].present?
+      sql_query = "name ILIKE :query OR event ILIKE :query"
+      @events = policy_scope(Event).where(sql_query, query: "%#{params[:query]}%")
+    else
+      @events = policy_scope(Event)
+    end
   end
 
   def show
@@ -10,18 +16,44 @@ class EventsController < ApplicationController
 
   def new
     # => GET    /events/new
+    @event = Event.new
+    authorize @event
   end
 
   def create
     # => POST   /events
+    @event = Event.new(event_params)
+    authorize @event
+    @event.user = current_user
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render :new
+    end
   end
 
   def delete
     # => GET    /events/:id/delete
+    @events = Event.all
+    set_event
   end
 
   def destroy
     # => DELETE /events/:id
+    @event.destroy
+    authorize @event
+    redirect_to events_path
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:id])
+    authorize @event
+  end
+
+  def event_params
+    params.require(:event).permit(:photo, :name, :user, :category, :address)
   end
 
 end
