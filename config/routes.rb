a4e05946e25b9571f8e15a6ff72b3fa97c56b2e7
Solene Duprat <solene.duprat@gmail.com>
2020-05-27 05:54:37 -0400

    Fixed a small error in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  get '/events/pending', to: 'events#my_events', as: 'my_events'
+  get '/events/:id/delete', to: 'events#delete', as: 'delete_event'
+
   resources :events, except: [:edit, :update] do
     resources :requests, only: [:new, :create]
   end
 
-  get '/events/:id/delete', to: 'events#delete', as: 'delete_event'
-  get '/events/pending', to: 'events#my_events', as: 'my_events'
-
+  get '/requests/:id/submitted', to: 'requests#submitted', as: 'submitted_request'
   get '/requests/incoming', to: 'requests#incoming', as: 'incoming_requests'
   resources :requests, only: [:show, :update]
-  get '/requests/:id/submitted', to: 'requests#submitted', as: 'submitted_request'
 
   get '/inbox', to: 'messages#index', as: 'messages'
   get '/inbox/:user_id', to: 'messages#new', as: 'new_message'
