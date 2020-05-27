b98dca7f739edeba7292c9c739fee7b266456f12
Solene Duprat <solene.duprat@gmail.com>
2020-05-27 13:51:25 -0400

    Cool login and navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -36,19 +36,28 @@ document.addEventListener('turbolinks:load', () => {
 
   // Navbar
 
-const body = document.getElementById('body');
-const openMenu = document.getElementById('menuControlOpen');
-const closeMenu = document.getElementById('menuControlClose');
+  const body = document.getElementById('body');
+  const openMenu = document.getElementById('menuControlOpen');
+  const closeMenu = document.getElementById('menuControlClose');
+  const dimBackground = document.getElementById('dim-background')
 
-openMenu.addEventListener('click', function(e) {
-body.classList.toggle('menu--active');
-});
+  openMenu.addEventListener('click', function(e) {
+  body.classList.toggle('menu--active');
+  dimBackground.classList.remove("hide-dim-background");
+  dimBackground.classList.add("show-dim-background");
+  });
 
-closeMenu.addEventListener('click', function(e) {
-body.classList.toggle('menu--active');
-});
+  closeMenu.addEventListener('click', function(e) {
+  body.classList.toggle('menu--active');
+  dimBackground.classList.remove("show-dim-background");
+  dimBackground.classList.add("hide-dim-background");
+  });
 
-
+  dimBackground.addEventListener('click', function(e) {
+  body.classList.toggle('menu--active');
+  dimBackground.classList.remove("show-dim-background");
+  dimBackground.classList.add("hide-dim-background");
+  });
 
 });
 
