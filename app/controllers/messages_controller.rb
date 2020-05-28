a1543d0fb89752cd2a9d5ee02f0b5c1761813c57
Samantha Andre <samantha.s.andre@gmail.com>
2020-05-28 11:51:45 -0400

    test

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,7 @@ class MessagesController < ApplicationController
   def new
     # => GET    /inbox/:user_id
     @message = Message.new
-    @event = current_user.requests.where(status: "Accepted").event
+    # @event = current_user.requests.where(status: "Accepted")
   end
 
   def create
