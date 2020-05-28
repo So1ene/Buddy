2a297b28d291caa6e5dbc05b51713265ee5b1530
Samantha Andre <samantha.s.andre@gmail.com>
2020-05-28 11:44:33 -0400

    adding event name

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,6 +8,7 @@ class MessagesController < ApplicationController
   def new
     # => GET    /inbox/:user_id
     @message = Message.new
+    @event = current_user.requests.where(status: "Accepted").event
   end
 
   def create
