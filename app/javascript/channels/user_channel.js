64c004302cdda841d9c521329a2aa1b47f9d5a20
Samantha Andre <samantha.s.andre@gmail.com>
2020-06-02 11:13:38 -0400

    instant messaging working

---
+++ app/javascript/channels/user_channel.js
@@ -0,0 +1,15 @@
+import consumer from "./consumer";
+
+const initUserChannelCable = () => {
+  const messagesContainer = document.getElementById('messages');
+  if (messagesContainer) {
+
+    consumer.subscriptions.create({ channel: "UserChannel" }, {
+      received(data) {
+        messagesContainer.insertAdjacentHTML("beforeend", data); // called when data is broadcast in the cable
+      },
+    });
+  }
+}
+
+export { initUserChannelCable };
