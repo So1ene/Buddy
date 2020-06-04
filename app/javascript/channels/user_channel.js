e29f96086e736737c0c3bd34ab898d29c2bcceae
Solene Duprat <solene.duprat@gmail.com>
2020-06-04 14:42:01 -0400

    fixed the chat scroll

--- app/javascript/channels/user_channel.js
+++ app/javascript/channels/user_channel.js
@@ -7,6 +7,7 @@ const initUserChannelCable = () => {
     consumer.subscriptions.create({ channel: "UserChannel" }, {
       received(data) {
         messagesContainer.insertAdjacentHTML("beforeend", data); // called when data is broadcast in the cable
+        window.scrollTo(0,document.body.scrollHeight);
       },
     });
   }
